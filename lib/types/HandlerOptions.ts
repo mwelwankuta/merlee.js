ad5df9abdce0805564257ce2a690f6be38363e49
Mwelwa Nkuta <mwelwankuta@gmail.com>
2021-12-10 17:21:59 +0200

    Remove types folder

--- lib/types/HandlerOptions.ts
+++
@@ -1,9 +0,0 @@
-/*
- add more methods 
-*/
-
-export interface HandlerOptions {
-  path: string;
-  method?: 'GET' | 'POST';
-  middleware?: Function;
-}
--- lib/types/index.ts
+++
@@ -1,5 +0,0 @@
-import { HandlerOptions } from './HandlerOptions';
-import { Options } from './Options';
-import { Request, Response } from './Response';
-
-export { HandlerOptions, Options, Request, Response };
